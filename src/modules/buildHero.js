aaf3b3901e2b95344da7f93e35eb72c98fce466c
Jacob Dowdle <jacobk.dowdle@gmail.com>
2022-09-18 14:08:03 +0800

    need to make a new box and grey

--- src/modules/buildHero.js
+++ src/modules/buildHero.js
@@ -2,6 +2,8 @@ function buildHero() {
     let htmlBody = document.getElementById('body');
     // create elements
     let backgroundImg = document.createElement('img');
+    //add id
+    backgroundImg.id = 'heroBackgroundImg';
     // add src
     backgroundImg.src = '../img/heroBackground.jpg';
     //append
