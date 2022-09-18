2e873e9fe576c8ed930c8da5b78b7ae9158cad23
Jacob Dowdle <jacobk.dowdle@gmail.com>
2022-09-18 14:21:43 +0800

    need to blend the hero img into the background

--- src/modules/buildHero.js
+++ src/modules/buildHero.js
@@ -1,13 +1,16 @@
 function buildHero() {
     let htmlBody = document.getElementById('body');
     // create elements
+    let heroContainer = document.createElement('div');
     let backgroundImg = document.createElement('img');
     //add id
+    heroContainer.id = 'heroContainer';
     backgroundImg.id = 'heroBackgroundImg';
     // add src
     backgroundImg.src = '../img/heroBackground.jpg';
     //append
-    htmlBody.append(backgroundImg);
+    heroContainer.append(backgroundImg);
+    htmlBody.append(heroContainer);
 }
 
 export { buildHero };
