39d4f157dc457b485a09343d6febc152236b7076
jakovid <jacobk.dowdle@gmail.com>
2022-09-20 15:13:24 +0800

    building drop down menus using objects

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import { buildHeader } from "./modules/buildHeader";
 import { buildHero } from "./modules/buildHero";
+import { productsSolutionsMenu } from "./modules/productsSolutionsMenu";
 
 (function(){
     var buildIbmClone = {
         init: function() {
             buildHeader();
-            buildHero();
+            // buildHero();
+            productsSolutionsMenu();
         }
     }
     buildIbmClone.init();
---
+++ src/modules/headerMenuInformation.js
@@ -0,0 +1,8 @@
+var productsSolutionsInfo = [
+    {itemName: 'The Essentials'},
+    {itemName: 'Top products & platfroms'},
+    {itemName: 'Industries'},
+    
+]
+
+export { productsSolutionsInfo };
