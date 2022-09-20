7607ace582254ebaf044eae149380a34ad794e5f
jakovid <jacobk.dowdle@gmail.com>
2022-09-20 15:21:53 +0800

    continued to build out the drop down menu function

--- src/modules/headerMenuInformation.js
+++ src/modules/headerMenuInformation.js
@@ -2,7 +2,17 @@ var productsSolutionsInfo = [
     {itemName: 'The Essentials'},
     {itemName: 'Top products & platfroms'},
     {itemName: 'Industries'},
-    
+    {itemName: 'Artificial intelligence'},
+    {itemName: 'Automation'},
+    {itemName: 'Blockchain'},
+    {itemName: 'Business operations'},
+    {itemName: 'Cloud computing'},
+    {itemName: 'Data & Analytics'},
+    {itemName: 'IT Infrastructure'},
+    {itemName: 'Security'},
+    {itemName: 'Supply chain'},
+    {itemName: 'Flexible payment plans'},
+    {itemName: 'View all products'}
 ]
 
 export { productsSolutionsInfo };
